1d12b4c4bc3919c8301c23ff431b60d6af0479b4
Alan Potter <alanjpotter@gmail.com>
2015-07-29 11:40:46 -0400

    Disable conjur-ui service so it doesn't start on reboot

--- recipes/_conjur_ui.rb
+++ recipes/_conjur_ui.rb
@@ -2,3 +2,13 @@ cookbook_file "/etc/init/conjur-ui.conf" do
   source "default/conjur-ui.upstart.conf"
   mode "0644"
 end
+
+execute "reload initctl config" do
+  command "initctl reload-configuration"
+end
+
+service 'conjur-ui' do
+  provider Chef::Provider::Service::Upstart
+  action :disable
+end
+
